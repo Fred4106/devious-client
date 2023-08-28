bf319cd06518883b4e6619eb571400743c05e304
Nathaniel <natruf@gmail.com>
2023-08-27 19:21:25 -0500

    workflows

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -25,9 +25,9 @@
 
 object ProjectVersions {
     const val launcherVersion = "1.0.0"
-    const val rlVersion = "1.10.10"
+    const val rlVersion = "1.10.10.3"
 
-    const val openosrsVersion = "4.31.2"
+//    const val openosrsVersion = "4.31.2"
 
     const val rsversion = 216
     const val cacheversion = 165
